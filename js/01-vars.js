b7fbafc93a7812eaf91ec5741ed305f8fc83b6e3
VTrofym <v.trof@icloud.com>
2022-08-12 00:09:23 +0300

    установил ESLint

--- js/01-vars.js
+++ js/01-vars.js
@@ -1,8 +1,13 @@
-const pricePerDroid = 800;
-const orderedQuantity = 6;
-const deliveryFee = 50;
-const totalPrice = pricePerDroid * orderedQuantity + deliveryFee;
-const message = 'You ordered droids worth' + totalPrice + 'credits. Delivery' + deliveryFee + 'credits is included in total price.'
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -29,6 +34,38 @@ const message = 'You ordered droids worth' + totalPrice + 'credits. Delivery' + 
 
 //----------------------
 // Функция
+
+// function makeOrderMessage(orderedQuantity, pricePerDroid, deliveryFee) {
+//   const totalPrice = orderedQuantity * pricePerDroid + deliveryFee;
+//   const message = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`;
+//   return message;
+// }
+// console.log(makeOrderMessage);
+
+// function calculateTotalPrice (orderedQuantity, pricePerItem) {
+//   const totalPrice = orderedQuantity * pricePerItem;
+//   return totalPrice;
+// };
+
+// function makeMessage (name, price) {
+//   const message =`You picked ${name}, price per item is ${price} credits`;
+//   return message;
+// };
+
+// function add(a, b, c) {
+// return a + b + c;
+// }
+// add(2, 5, 8); // 15
+// console.log(add(15, 27, 10));
+// console.log(add(10, 20, 30));
+// console.log(add(5, 10, 15));
+
+// function add(a, b, c) {
+//   console.log(`Addition result equals ${a + b + c}`);
+// }
+// add(15, 27, 10);
+// add(10, 20, 30);
+// add(5, 10, 15);
 
 // function sayHi() {
 //   console.log("Hello, this is my first function!");
